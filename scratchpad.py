6d522a728c20711b8aeca489b1927dd009567e06
Rogério Yamada <eyamrog@gmail.com>
2024-08-07 18:13:04 -0300

    20240807-1812

--- scratchpad.py
+++
@@ -1,6 +0,0 @@
-yt-dlp --write-auto-sub --sub-lang pt --sub-format vtt --skip-download https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
-
-yt-dlp --skip-download --write-auto-sub --sub-lang pt https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
-
-yt-dlp --write-auto-sub --sub-lang pt  --skip-download https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
-
