e8821044a835f2b95f1b75e9929d659893558d4d
eyamrog <eyamrog@gmail.com>
2024-08-07 06:22:19 -0300

    Update scratchpad.py

--- scratchpad.py
+++ scratchpad.py
@@ -1,1 +1,6 @@
-yt-dlp --write-auto-sub --sub-format vtt --skip-download https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
+yt-dlp --write-auto-sub --sub-lang pt --sub-format vtt --skip-download https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
+
+yt-dlp --skip-download --write-auto-sub --sub-lang pt https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
+
+yt-dlp --write-auto-sub --sub-lang pt  --skip-download https://youtu.be/QiyQHUGgmi4?si=NqkPQl3I0h-bPuyt
+
